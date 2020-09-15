1475af9b0e15e5c951fc9a0e9d093149c3bd5ca7
KrokoOnAir <krokoonair@gmail.com>
2020-09-15 21:59:00 +0200

    Added Fly and fixxed TabGUI

--- koks/module/impl/movement/Fly.java
+++ koks/module/impl/movement/Fly.java
@@ -1,5 +1,6 @@
 package koks.module.impl.movement;
 
+import koks.api.settings.Setting;
 import koks.event.Event;
 import koks.event.impl.EventUpdate;
 import koks.module.Module;
@@ -10,14 +11,25 @@ import koks.module.Module;
  */
 public class Fly extends Module {
 
+    public Setting aac3312boost = new Setting("AAC3.3.12-Boost", 9F, 1F, 10F, true, this);
+    public Setting mode = new Setting("Mode", new String[]{"AAC3.3.12"}, "AAC3.3.12", this);
+
     public Fly() {
         super("Fly", "Flying around the world", Category.MOVEMENT);
+        registerSetting(aac3312boost);
+        registerSetting(mode);
     }
 
     @Override
     public void onEvent(Event event) {
-        if (event instanceof EventUpdate) {
-            mc.thePlayer.motionY = 0;
+        switch (mode.getCurrentMode()) {
+            case "AAC3.3.12":
+                if (event instanceof EventUpdate) {
+                    if (mc.thePlayer.posY <= -70) {
+                        mc.thePlayer.motionY = aac3312boost.getCurrentValue();
+                    }
+                }
+                break;
         }
     }
 
